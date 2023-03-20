7d2bc8d7b8c81ac2ed75042ed85fd6413c97a16c
Stefan0712 <s.vladulescu@gmail.com>
2023-03-20 23:31:41 +0200

    Moved to express and added video page. Hosted videos on Dropbox

---
+++ data.js
@@ -0,0 +1,78 @@
+const { defaults } = require("request")
+
+const videos = [
+    {
+      id: 0,
+      name: "I took them from behind",
+      videoUrl: 'https://www.dropbox.com/s/d9ukdu7uu1ac3pt/video-1.mkv?raw=1',
+      previewUrl: '/images/img-1.png',
+      views: 1.1,
+      likes: 69,
+      likeNr: 412,
+      dislikeNr:51,
+      savedNr: 4,
+      posted: 5
+    },
+    {
+      id: 1,
+      name: "Taking multiple guys at once",
+      videoUrl: 'https://www.dropbox.com/s/5oqd10xza7tjo7z/video-2.mkv?raw=1',
+      previewUrl: '/images/img-2.png',
+      views: 1.1,
+      likes: 69,
+      likeNr: 412,
+      dislikeNr:51,
+      savedNr: 4,
+      posted: 5
+    },
+    {
+      id: 2,
+      name: "The tehnique is important, not Deek's size",
+      videoUrl: 'https://www.dropbox.com/s/oodqgdeerwrcufp/video-3.mkv?raw=1',
+      previewUrl: '/images/img-3.png',
+      views: 1.1,
+      likes: 69,
+      likeNr: 412,
+      dislikeNr:51,
+      savedNr: 4,
+      posted: 5
+    },
+    {
+      id: 3,
+      name: "He was so good that I died",
+      videoUrl: 'https://www.dropbox.com/s/0991pkp86382d30/video-4.mkv?raw=1',
+      previewUrl: '/images/img-4.png',
+      views: 1.1,
+      likes: 69,
+      likeNr: 412,
+      dislikeNr:51,
+      savedNr: 4,
+      posted: 5
+    },
+    {
+      id: 4,
+      name: "Can't believe how big both of them were",
+      videoUrl: 'https://www.dropbox.com/s/3sv9a84unq4290o/video-5.mkv?raw=1',
+      previewUrl: '/images/img-5.png',
+      views: 1.1,
+      likes: 69,
+      likeNr: 412,
+      dislikeNr:51,
+      savedNr: 4,
+      posted: 5
+    },
+    {
+      id: 5,
+      name: "It's so fucking huge",
+      videoUrl: "https://www.dropbox.com/s/s6qxdkuttlihs52/video-6.mp4?raw=1",
+      previewUrl: '/images/img-6.png',
+      views: 1.1,
+      likes: 69,
+      likeNr: 412,
+      dislikeNr:51,
+      savedNr: 4,
+      posted: 5
+    }
+  ]
+  const tags = ['Japanese','Dancer','Gamer','Wizard','Cat Girl','Anime Girl','American','Moe Moe','Lag on Hogwarts Legacy', "Please don't ban me", "Is it too late to say that I'm sorry?"]
+  module.exports = {tags, videos}
---
+++ index.js
@@ -0,0 +1,41 @@
+const express = require('express')
+const app = express();
+const data = require('./data.js')
+const ejsMate = require('ejs-mate')
+const path = require('path')
+
+app.engine('ejs', ejsMate)
+app.set('view engine','ejs')
+app.set('views', path.join(__dirname, 'views'))
+app.use(express.static(__dirname + '/public'))
+
+
+
+
+
+
+
+
+app.get('/',(req, res)=>{
+    res.render('index', {data})
+})
+app.get('/video/:id',(req, res)=>{
+    const vidId = req.params.id;
+    const videos = data.videos;
+    let video;
+    for(let i=0;i<videos.length;i++){
+        if(vidId==videos[i].id){
+            video=videos[i]
+        }
+    }
+    res.render('showVideo',{video, videos})
+})
+app.get('*',(req,res)=>{
+    res.render('notFound')
+})
+
+
+
+app.listen(3000,()=>{
+    console.log('The server is running on port 3000!')
+})
